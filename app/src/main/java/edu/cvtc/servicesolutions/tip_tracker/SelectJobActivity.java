862ed3b8aa4a549cce32670a1f90cf6a0e1e433f
Ric Lucas <98325951+riclpicl@users.noreply.github.com>
2022-04-25 11:47:52 -0500

    creates custom toolbar for adding a job, starting input/output of jobs entered

--- app/src/main/java/edu/cvtc/servicesolutions/tip_tracker/SelectJobActivity.java
+++ app/src/main/java/edu/cvtc/servicesolutions/tip_tracker/SelectJobActivity.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,35 +24,24 @@ import cvc.edu.servicesolutions_tiptracker.databinding.ActivitySelectJobBinding;
 
 public class SelectJobActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivitySelectJobBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_job);
 
-        binding = ActivitySelectJobBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        Toolbar jobToolbar = (Toolbar) findViewById(R.id.add_job_toolbar);
+        jobToolbar.setTitle("Add a Job");
+        jobToolbar.setNavigationIcon(R.drawable.ic_add_job);
+        setSupportActionBar(jobToolbar);
 
-        setSupportActionBar(binding.toolbar);
+        jobToolbar.setNavigationOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(SelectJobActivity.this, "Add a Job Toolbar Image Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_select_job);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_select_job);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
