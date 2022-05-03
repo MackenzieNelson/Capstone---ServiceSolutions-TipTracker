ee219212f7c085fb0081d97f5be7c209309dd39e
Carter Jensen <CoderJensen@users.noreply.github.com>
2022-05-02 21:29:08 -0500

    [Finishes #181916587] Add ability to view tips

--- app/src/main/java/edu/cvtc/servicesolutions/tip_tracker/TipRecordActivity.java
+++ app/src/main/java/edu/cvtc/servicesolutions/tip_tracker/TipRecordActivity.java
@@ -1,4 +1,130 @@
 package edu.cvtc.servicesolutions.tip_tracker;
 
-public class TipRecordActivity {
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TipRecordActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    // Constants
+    public static final int ITEM_TIPS = 0;
+    public static final int LOADER_TIPS = 0;
+
+    // Member Variables
+    private JobOpenHelper mDbOpenHelper;
+    private RecyclerView mRecyclerItems;
+    private LinearLayoutManager mTipsLayoutManager;
+    private TipRecylcerAdapter mTipRecyclerAdapter;
+
+    // Boolean to check if then 'onCreateLoader' method has run
+    private boolean mIsCreated = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_tip);
+
+        mDbOpenHelper = new JobOpenHelper(this);
+        initializeDisplayContent();
+    }
+
+    private void initializeDisplayContent() {
+        // Retrieve the information from your database
+        DataManager.loadFromDatabase(mDbOpenHelper);
+
+        // Set a reference to list of tips layout
+        mRecyclerItems = (RecyclerView) findViewById(R.id.list_tips);
+        mTipsLayoutManager = new LinearLayoutManager(this);
+
+        // No cursor yet, so pass null
+        mTipRecyclerAdapter = new TipRecylcerAdapter(this, null);
+
+        // Display the tips
+        displayTips();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // use restartLoader instead of initLoader to make sure
+        // you re-query the database each time the activity is
+        // loaded in the app
+        LoaderManager.getInstance(this).restartLoader(LOADER_TIPS, null, this);
+    }
+
+    private void displayTips() {
+        mRecyclerItems.setLayoutManager(mTipsLayoutManager);
+        mRecyclerItems.setAdapter(mTipRecyclerAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mDbOpenHelper.close();
+        super.onDestroy();
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        CursorLoader loader = null;
+
+        if (id == LOADER_TIPS) {
+            loader = new CursorLoader(this) {
+                @Override
+                public Cursor loadInBackground () {
+                    mIsCreated = true;
+                    // open database in read mode
+                    SQLiteDatabase db = mDbOpenHelper.getReadableDatabase();
+
+                    // create list of columns to be returned
+                    String[] tipColumns = {
+                            JobsDatabaseContract.JobInfoEntry.COLUMN_HOURLY_RATE,
+                            JobsDatabaseContract.JobInfoEntry.COLUMN_HOURS_WORKED,
+                            JobsDatabaseContract.JobInfoEntry.COLUMN_CASH_TIPS,
+                            JobsDatabaseContract.JobInfoEntry.COLUMN_CREDIT_TIPS,
+                            JobsDatabaseContract.JobInfoEntry.COLUMN_DATE,
+                            JobsDatabaseContract.JobInfoEntry._ID };
+
+                    // create an order by field for sorting
+                    String tipOrderBy = JobsDatabaseContract.JobInfoEntry.COLUMN_DATE;
+
+                    // populate cursor with results
+                    return db.query(JobsDatabaseContract.JobInfoEntry.TABLE_NAME,tipColumns,
+                            null, null, null, null,
+                            tipOrderBy);
+                }
+            };
+        }
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        if (loader.getId() == LOADER_TIPS && mIsCreated) {
+            // associate cursor with RecyclerAdapter
+            mTipRecyclerAdapter.changeCursor(data);
+            mIsCreated = false;
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        if (loader.getId() == LOADER_TIPS) {
+            // change the cursor to null (cleanup)
+            mTipRecyclerAdapter.changeCursor(null);
+        }
+    }
+
 }
---
+++ app/src/main/java/edu/cvtc/servicesolutions/tip_tracker/TipRecylcerAdapter.java
@@ -0,0 +1,136 @@
+package edu.cvtc.servicesolutions.tip_tracker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Date;
+
+public class TipRecylcerAdapter extends RecyclerView.Adapter<TipRecylcerAdapter.ViewHolder> {
+
+    // Member variables
+    private final Context mContext;
+    private final LayoutInflater mLayoutInflater;
+    private Cursor mCursor;
+
+    private int mIdPosition;
+    private int mOriginalHoursWorkedPosition;
+    private int mOriginalHourlyRatePosition;
+    private int mOriginalCashTipPosition;
+    private int mOriginalCreditTipPosition;
+    private int mOriginalDatePosition;
+
+    public TipRecylcerAdapter(Context context, Cursor cursor) {
+        mContext = context;
+        mCursor = cursor;
+        mLayoutInflater = LayoutInflater.from(context);
+
+        // Used to get the positions of the columns we
+        // are interested in
+        populateColumnPositions();
+    }
+
+    private void populateColumnPositions() {
+        if (mCursor != null) {
+            // get column indexes from mCursor
+            mIdPosition = mCursor.getColumnIndex(JobsDatabaseContract.JobInfoEntry._ID);
+            mOriginalHoursWorkedPosition = mCursor.getColumnIndex(JobsDatabaseContract.JobInfoEntry.COLUMN_HOURLY_RATE);
+            mOriginalHourlyRatePosition = mCursor.getColumnIndex(JobsDatabaseContract.JobInfoEntry.COLUMN_HOURS_WORKED);
+            mOriginalCashTipPosition = mCursor.getColumnIndex(JobsDatabaseContract.JobInfoEntry.COLUMN_CASH_TIPS);
+            mOriginalCreditTipPosition = mCursor.getColumnIndex(JobsDatabaseContract.JobInfoEntry.COLUMN_CREDIT_TIPS);
+            mOriginalDatePosition = mCursor.getColumnIndex(JobsDatabaseContract.JobInfoEntry.COLUMN_DATE);
+        }
+    }
+
+    public void changeCursor(Cursor cursor) {
+        // If the cursor is open, close it
+        if (mCursor != null) {
+            mCursor.close();
+        }
+
+        // Create a new cursor based upon
+        // the object passed in.
+        mCursor = cursor;
+
+        // Get the positions of the columns in
+        // your cursor.
+        populateColumnPositions();
+
+        // Tell the activity that the data set
+        // has changed.
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mLayoutInflater.inflate(R.layout.tip_list, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Move the cursor to the correct row
+        mCursor.moveToPosition(position);
+
+        // get the actual values
+        int id = mCursor.getInt(mIdPosition);
+        double originalHoursWorked = mCursor.getDouble(mOriginalHoursWorkedPosition);
+        double originalHourlyRate = mCursor.getDouble(mOriginalHourlyRatePosition);
+        double originalCashTip = mCursor.getDouble(mOriginalCashTipPosition);
+        double originalCreditTip = mCursor.getDouble(mOriginalCreditTipPosition);
+        // Using string until I find an alternative for date.
+        String originalDate = mCursor.getString(mOriginalDatePosition);
+
+
+        // pass the info
+        holder.mId = id;
+        holder.mOriginalHoursWorked.setText(String.valueOf(mOriginalHoursWorkedPosition));
+        holder.mOriginalHourlyRate.setText(String.valueOf(mOriginalHourlyRatePosition));
+//        holder.mOriginalCashTip.setText(String.valueOf(mOriginalCashTipPosition));
+//        holder.mOriginalCreditTip.setText(String.valueOf(mOriginalCreditTipPosition));
+//        holder.mOriginalDate.setText(originalDate);
+    }
+
+    @Override
+    public int getItemCount() {
+        // if the cursor is null, return 0. Otherwise
+        // return the count of records
+        return mCursor == null ? 0 : mCursor.getCount();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Member variables for inner class
+        public int mId;
+        public final TextView mOriginalHoursWorked;
+        public final TextView mOriginalHourlyRate;
+//        public final TextView mOriginalCashTip;
+//        public final TextView mOriginalCreditTip;
+//        public final TextView mOriginalDate;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mOriginalHoursWorked = (TextView) itemView.findViewById(R.id.income_hours_worked);
+            mOriginalHourlyRate = (TextView) itemView.findViewById(R.id.income_hourly_rate);
+//            mOriginalCashTip = (TextView) itemView.findViewById(R.id.income_);
+//            mOriginalCreditTip = (TextView) itemView.findViewById(R.id.income_);
+//            mOriginalDate = (TextView) itemView.findViewById(R.id.income_);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(mContext, IncomeActivity.class);
+                    intent.putExtra(IncomeActivity.INCOME_ID, mId);
+                    mContext.startActivity(intent);
+                }
+            });
+        }
+    }
+}
