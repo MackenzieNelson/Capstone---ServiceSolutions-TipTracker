2c559fdb48935a9c8ca1ca0aaf401404aa0f5bfc
Mackenzie Nelson <kenzie2397@yahoo.com>
2022-05-02 19:40:11 -0500

    start of calculation activity

--- app/src/main/java/edu/cvtc/servicesolutions/tip_tracker/IncomeCalculationsActivity.java
+++ app/src/main/java/edu/cvtc/servicesolutions/tip_tracker/IncomeCalculationsActivity.java
@@ -1,4 +1,48 @@
 package edu.cvtc.servicesolutions.tip_tracker;
 
-public class IncomeCalculationsActivity {
+import static edu.cvtc.servicesolutions.tip_tracker.IncomeActivity.LOADER_INCOME;
+
+import android.database.Cursor;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+
+public class IncomeCalculationsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private JobOpenHelper mDbOpenHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_income_calculations);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        mDbOpenHelper = new JobOpenHelper(this);
+
+        LoaderManager.getInstance(this).initLoader(LOADER_INCOME, null, this);
+
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
 }
