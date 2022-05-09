3298df6b98610b70e1327e0ada8ea912df32603f
Naomi Smith <noesau@student.cvtc.edu>
2022-05-09 15:27:51 -0500

    [Finishes #181916653] Allow user to use dark mode.

--- app/src/main/java/edu/cvtc/servicesolutions/tip_tracker/JobMenuActivity.java
+++ app/src/main/java/edu/cvtc/servicesolutions/tip_tracker/JobMenuActivity.java
@@ -1,17 +1,29 @@
 package edu.cvtc.servicesolutions.tip_tracker;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toolbar;
 
-public class JobMenuActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class JobMenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_menu);
+
     }
 
 
@@ -30,4 +42,10 @@ public class JobMenuActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
 }
